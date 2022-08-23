7cb123910018d4fbd0e687ccc3362ecb961ba954
BiniyaPD <110925626+BiniyaPD@users.noreply.github.com>
2022-08-23 11:52:49 +0530

    Add files via upload

---
+++ TicketTracking.CommonLayer/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketTracking.CommonLayer.Models
+{
+    public class Employee
+    {
+        public string Employeeid { get; set; }
+        public string EmployeeName { get; set; }
+        public DateTime? HireDate { get; set; }
+        public string Dept { get; set; }
+        public string Epassword { get; set; }
+    }
+}
---
+++ TicketTracking.CommonLayer/Models/Ticket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketTracking.CommonLayer.Models
+{
+    public class Ticket
+    {
+        public int TicketId { get; set; }
+        public string LoggedBy { get; set; }
+        public DateTime? RaisedDate { get; set; }
+        public string Severity { get; set; }
+        public string Tdescription { get; set; }
+        public string ResolvedBy { get; set; }
+        public string Resolution { get; set; }
+        public DateTime? ResolvedDate { get; set; }
+        public string TicketStatus { get; set; }
+
+        public virtual Employee LoggedByNavigation { get; set; }
+        public virtual Employee ResolvedByNavigation { get; set; }
+    }
+}
